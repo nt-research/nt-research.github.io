aa145a034fe08f7903494cb7bdd488876baeadf2
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2024-10-19 21:02:12 +0200

    style: Comment out unused blog link in config settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -123,10 +123,10 @@ const config: Config = {
         {
           title: "More",
           items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
+            // {
+            //   label: "Blog",
+            //   to: "/blog",
+            // },
             {
               label: "GitHub",
               href: "https://github.com/nt-research/nt-research.github.io",
